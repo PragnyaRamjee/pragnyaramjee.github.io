9dcb61abcb6ebbb2a291bbd45f8be01d80e1a27e
Pragnya Ramjee <t-pramjee@microsoft.com>
2024-05-03 10:34:20 +0530

    Updates

---
+++ component---src-pages-index-js-3c6b0d3252abf7ac6841.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[678],{558:function(e,t,l){l.r(t),l.d(t,{default:function(){return u}});var r=l(294);function u(){return r.createElement("div",null,"Farewell, you ugly toad!")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-3c6b0d3252abf7ac6841.js.map
